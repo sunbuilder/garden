d89805cba67f9c701592ba9bb9618a23d38c908c
Tonykui <970745326@qq.com>
2019-04-17 21:14:31 +0800

    1
    1

--- project/pages/detail/detail.js
+++ project/pages/detail/detail.js
@@ -4,52 +4,66 @@ Page({
 
   },
   reg: function (e) {
-
-    // wx.showModal({
-    //   title: '提示',
-    //   content: '确定要删除吗？',
-    //   success: function (sm) {
-    //     if (sm.confirm) {
-    //       // 用户点击了确定 可以调用删除方法了
-    //     } else if (sm.cancel) {
-    //       console.log('用户点击取消')
-    //     }
-    //   }
-    // })
-    var plantname = e.detail.value.plantname;
-    var diarymethod = e.detail.value.diarymethod;
-    var diaryspace = e.detail.value.diaryspace;
-    var diarycity = e.detail.value.diarycity;
-    var userid = wx.getStorageSync("openid")
-    var diaryid = this.data.diary.diaryId;
-    wx.request({
-      url: getApp().globalData.path + 'updateDiary' + getApp().globalData.path2,
-      method: "POST",
-      data: {
-        diary: {
-          diaryUserid: userid,
-          diaryMethod: diarymethod,
-          diaryPlantname: plantname,
-          diarySpace: diaryspace,
-          diaryCity: diarycity,
-          diaryUserid: userid,
-          diaryId: diaryid
-        }
-     
-
-      },
-         success: function (res) {
-
-
-        if (res.data == "ok") {
-
-         
-          wx.reLaunch({
-            url: '../index/index?msg='+"修改日记信息成功"
+var that=this;
+    wx.showModal({
+      title: '提示',
+      content: '确定要删除吗？',
+      success: function (sm) {
+        var they=that;
+        if (sm.confirm) {
+          // 用户点击了确定 可以调用删除方法了
+          wx.request({
+            url: getApp().globalData.path + 'deleteDiary' + getApp().globalData.path2,
+            data: {
+              diaryid: they.data.diary.diaryId
+            },
+            success: function (res) {
+             if(res.data=="ok"){
+               wx.reLaunch({
+                 url: '../index/index',
+               })
+             }
+            }
           })
+        } else if (sm.cancel) {
+          console.log('用户点击取消')
         }
       }
     })
+    // var plantname = e.detail.value.plantname;
+    // var diarymethod = e.detail.value.diarymethod;
+    // var diaryspace = e.detail.value.diaryspace;
+    // var diarycity = e.detail.value.diarycity;
+    // var userid = wx.getStorageSync("openid")
+    // var diaryid = this.data.diary.diaryId;
+    // wx.request({
+    //   url: getApp().globalData.path + 'updateDiary' + getApp().globalData.path2,
+    //   method: "POST",
+    //   data: {
+    //     diary: {
+    //       diaryUserid: userid,
+    //       diaryMethod: diarymethod,
+    //       diaryPlantname: plantname,
+    //       diarySpace: diaryspace,
+    //       diaryCity: diarycity,
+    //       diaryUserid: userid,
+    //       diaryId: diaryid
+    //     }
+     
+
+    //   },
+    //      success: function (res) {
+
+
+    //     if (res.data == "ok") {
+
+         
+    //       wx.reLaunch({
+    //         url: '../index/index?msg='+"修改日记信息成功"
+    //       })
+    //     }
+    //   }
+    // })
   },
   onLoad: function (option) {
     var id = option.diaryid;
